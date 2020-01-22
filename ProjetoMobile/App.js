9bd6f06fdc0fb49e35b3c025fdf47f1727ee88aa
GEODATIN <allanpereira016@gmail.com>
2020-01-22 13:17:47 -0300

    Construindo a tela Main

--- ProjetoMobile/App.js
+++ ProjetoMobile/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <Text>Hello WOrld</Text>
     </View>
   );
 }
@@ -17,3 +19,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
---
+++ ProjetoMobile/src/pages/Main.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {View, Text} from "react-native";
+import api from "../services/api";
+
+function Main(){
+    console.log("Main Executou")
+    async function loadDevs(){
+        const response = await api.get("/api/municipios");
+        console.log(response);
+    }
+    loadDevs();
+    return(
+        <View>
+            <Text>
+                Hello World
+            </Text>
+        </View>
+    )  
+}
+export default Main;
