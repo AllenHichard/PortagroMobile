f9679571b707dcf40edbdada76fc7b23598294c9
AllenHichard <53483318+AllenHichard@users.noreply.github.com>
2020-01-31 12:01:09 -0300

    Colocando scroll e imagens na tela de quem somos

--- ProjetoMobile/src/pages/QuemSomos.js
+++ ProjetoMobile/src/pages/QuemSomos.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {View, Text, StyleSheet, Image} from "react-native";
+import {View, Text, StyleSheet, Image, SafeAreaView, ScrollView} from "react-native";
 import { createStackNavigator } from 'react-navigation-stack';
 import css from "../utils/css/css"
 
@@ -25,11 +25,28 @@ render() {
 </Text>
 </View>
 */
-function carregarEquipe() {
-  const [Images, setImages] = useState([]);
-  for(let x=1; x<4; x++){
-    setImages([...Images, <Image source={require(`../utils/pessoas/${x}.jpg`)} />])
-  }
+async function carregarEquipe() {
+  //const [Images, setImages] = useState([]);
+  const Images = []
+  Images.push((<Image source={require('../utils/pessoas/'+1+'.jpg')} />))
+  Images.push((<Image source={require('../utils/pessoas/'+2+'.jpg')} />))
+  Images.push((<Image source={require('../utils/pessoas/'+3+'.jpg')} />))
+  Images.push((<Image source={require('../utils/pessoas/'+4+'.jpg')} />))
+  Images.push((<Image source={require('../utils/pessoas/'+5+'.jpg')} />))
+  Images.push((<Image source={require('../utils/pessoas/'+6+'.jpg')} />))
+  Images.push((<Image source={require('../utils/pessoas/'+7+'.jpg')} />))
+  Images.push((<Image source={require('../utils/pessoas/'+8+'.jpg')} />))
+  Images.push((<Image source={require('../utils/pessoas/'+9+'.jpg')} />))
+  Images.push((<Image source={require('../utils/pessoas/'+10+'.jpg')} />))
+  Images.push((<Image source={require('../utils/pessoas/'+11+'.jpg')} />))
+  Images.push((<Image source={require('../utils/pessoas/'+12+'.jpg')} />))
+  Images.push((<Image source={require('../utils/pessoas/'+13+'.jpg')} />))
+  Images.push((<Image source={require('../utils/pessoas/'+14+'.jpg')} />))
+  Images.push((<Image source={require('../utils/pessoas/'+15+'.jpg')} />))
+  Images.push((<Image source={require('../utils/pessoas/'+1+'.png')} />))
+  Images.push((<Image source={require('../utils/pessoas/'+2+'.png')} />))
+  Images.push((<Image source={require('../utils/pessoas/'+3+'.png')} />))
+  Images.push((<Image source={require('../utils/pessoas/'+4+'.png')} />))
   console.log(Images)
 }
 
@@ -44,17 +61,29 @@ class QuemSomos extends React.Component {
     render(){
       carregarEquipe();
       return(
-        <View style={styles.Background}> 
-                <Text style={styles.Title}>
-                    EQUIPE DE SUPERVISÃO, IDEALIZAÇÃO E APOIO INSTITUCIONAL
-                </Text>
-                <Text style={styles.SubTitle}>
-                  O Governo do Estado da Bahia, por iniciativa da Secretaria da Agricultura, Pecuária, Irrigação, Pesca e Aquicultura - SEAGRI, 
-                  em colaboração com a Secretaria de Ciência Tecnologia e Inovação - SECTI idealizou o Projeto Sistema de Informações Agropecuárias 
-                  do Estado da Bahia, elaborado e desenvolvido pela PPGM/UEFS. O projeto congrega uma equipe colaborativa que reúne Secretarias de Estado, 
-                  Universidade e Empresa de Tecnologia.
-                </Text>
-        </View>
+        <>
+           <SafeAreaView style={styles.container}>
+              <ScrollView style={styles.scrollView}  showsVerticalScrollIndicator={false}>
+                  <View style={styles.Background}> 
+                          <Text style={styles.Title}>
+                              EQUIPE DE SUPERVISÃO, IDEALIZAÇÃO E APOIO INSTITUCIONAL
+                          </Text>
+                          <Text style={styles.SubTitle}>
+                            O Governo do Estado da Bahia, por iniciativa da Secretaria da Agricultura, Pecuária, Irrigação, Pesca e Aquicultura - SEAGRI, 
+                            em colaboração com a Secretaria de Ciência Tecnologia e Inovação - SECTI idealizou o Projeto Sistema de Informações Agropecuárias 
+                            do Estado da Bahia, elaborado e desenvolvido pela PPGM/UEFS. O projeto congrega uma equipe colaborativa que reúne Secretarias de Estado, 
+                            Universidade e Empresa de Tecnologia.
+                          </Text>
+                  </View>
+                  <View style={styles.Background}>
+                        <Image source={require('../utils/pessoas/'+1+'.jpg')}></Image> 
+                        <Image source={require('../utils/pessoas/'+2+'.jpg')}></Image>
+                        <Image source={require('../utils/pessoas/'+3+'.jpg')}></Image>   
+                  </View>
+              </ScrollView>
+          </SafeAreaView>
+        </>
+
       )
     }
   
@@ -73,7 +102,15 @@ class QuemSomos extends React.Component {
 
 
 const styles = StyleSheet.create({
-  
+  container: {
+    flex: 1,
+  },
+  scrollView: {
+    backgroundColor: 'pink',
+    marginHorizontal: 20,
+  },
+
+
   Title:{
       alignItems: "center",
       justifyContent: "center",
